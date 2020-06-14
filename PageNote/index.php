b704e0e209cae2560314528eb1de4776b26db036
MisterX25 <Xavier.Carrel@cpnv.ch>
2020-06-14 17:50:29 +0200

    Exercice "Page pour insérer les notes" corrigé

--- PageNote/index.php
+++ PageNote/index.php
@@ -6,27 +6,19 @@
  **/
 
 require_once "database.php";
-if (isset($_POST["store"])) {
-    extract($_POST); // idEval, idStudent, $gradeValue
-    $newGrade = insert("Insert into grade (gradeValue,fkStudent,fkEval) values (:grade,:student,:eval)",
-        [
-            "grade" => $gradeValue,
-            "student" => $idStudent,
-            "eval" => $idEval
-        ]);
-}
+
+// Lire les données dont la vue aura besoin pour créer le formulaire
 
 $students = selectMany("Select * from person where role=0", []);
-$evals = selectMany("Select testDescription, moduleShortName from evaluation 
+$evals = selectMany("Select idEvaluation, testDescription, moduleShortName from evaluation 
 	inner join moduleinstance on fkModuleInstance = idModuleInstance 
     inner join module on fkModule = idModule", []);
+
+// et maintenant on peut construire le formulaire...
 ?>
 
 <div>
-    <p>Note ajoutée: <?= $newGrade ?></p>
-</div>
-<div>
-    <form method="post">
+    <form method="post" action="addgrade.php">
         Evaluation: <select name="idEval">
             <?php foreach ($evals as $eval) { ?>
                 <option value="<?= $eval['idEvaluation'] ?>"><?= $eval['testDescription'] ?>, module <?= $eval['moduleShortName'] ?></option>
